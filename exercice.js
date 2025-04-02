f90dd9d4ea3ddcacf4bfa82e3c8c7e90f12e5b3c
josuebyamungu <byamungujosue7@gmail.com>
2025-04-02 12:25:23 +0200

    some modifications done

---
+++ exercice.js
@@ -0,0 +1,13 @@
+let math=10;
+let français=11;
+let histoire=9;
+let geographie=7;
+let info=14;
+let max=80;
+let somme=math+français+histoire+geographie+info;
+let moyenne=somme/5;
+let pourcentage=somme*100/max;
+console.log(somme)
+console.log(moyenne)
+console.log("the pourcentage is"+pourcentage)
+console.log(pourcentage)
---
+++ script.js
@@ -0,0 +1,5 @@
+let Age=17;
+let gloire=25;
+let thierry=13;
+let ourages=Age+gloire+thierry;
+console.log(ourages);
